21e606b2b5787523baec84569d4de1d3acf007bb
Ace Olszowka <aolszowka@gmail.com>
2015-07-15 09:21:08 -0600

    * Initial commit of the HipChat v2 API CruiseControl.NET Publisher

---
+++ ccnet.hipchat.v2.plugin.tests/HipChatPublisherTests.cs
@@ -0,0 +1,137 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HipChatPublisherTests.cs" company="Ace Olszowka">
+// Copyright (c) 2015 Ace Olszowka.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ccnet.hipchat.v2.plugin.tests
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using HipChat.Net.Models.Request;
+    using Moq;
+    using NUnit.Framework;
+    using ThoughtWorks.CruiseControl.Core;
+
+    /// <summary>
+    /// Unit tests for the <see cref="HipChatPublisher"/> class.
+    /// </summary>
+    [TestFixture]
+    public class HipChatPublisherTests
+    {
+        [TestCaseSource(typeof(EvaluateIntegration_ValidInput_Tests)), Ignore("Broken because third party does not implement Equals")]
+        public void EvaluateIntegration_ValidInput(IIntegrationResult integrationResult, bool notifyOnlyOnError, Tuple<bool, SendNotification> expected)
+        {
+            Tuple<bool, SendNotification> actual = HipChatPublisher.EvaluateIntegration(integrationResult, notifyOnlyOnError);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+
+    /// <summary>
+    /// The Test Factory for the <see cref="EvaluateIntegration_ValidInput"/> tests.
+    /// </summary>
+    internal class EvaluateIntegration_ValidInput_Tests : IEnumerable
+    {
+        /// <summary>
+        /// The name used in several of the below tests.
+        /// </summary>
+        const string BuildName = "SomeBuildName With Spaces";
+
+        /// <summary>
+        /// The Build Successful SendNotification for several of the tests.
+        /// </summary>
+        private SendNotification BuildSuccessfulSendNotification =
+            new SendNotification()
+            {
+                Color = MessageColor.Green,
+                Message = "<b>[SUCCESSFUL]</b> SomeBuildName With Spaces",
+                Notify = true
+            };
+
+        private SendNotification BuildFixedSendNotification =
+            new SendNotification()
+            {
+                Color = MessageColor.Green,
+                Message = "<b>[FIXED]</b> SomeBuildName With Spaces",
+                Notify = true
+            };
+
+        private string[] BuildFailureSingleUser = new string[] { "bob" };
+
+        private string[] BuildFailureMultiUser = new string[] { "bob", "alice" };
+
+        const string BuildFailureUrl = "http://example.com";
+
+        private SendNotification BuildFailedSingleSendNotification =
+                        new SendNotification()
+                        {
+                            Color = MessageColor.Red,
+                            Message = "<b>[FAILED]</b> SomeBuildName With Spaces <a href=\"http://example.com\">Build Log</a>. Breakers: @bob",
+                            Notify = true
+                        };
+
+        private SendNotification BuildFailedMultiSendNotification =
+                new SendNotification()
+                {
+                    Color = MessageColor.Red,
+                    Message = "<b>[FAILED]</b> SomeBuildName With Spaces <a href=\"http://example.com\">Build Log</a>. Breakers: @bob,@alice",
+                    Notify = true
+                };
+
+        public IEnumerator GetEnumerator()
+        {
+            // Success Cases
+            yield return new TestCaseData(MockIIntegrationResultSuccess(BuildName), false, new Tuple<bool, SendNotification>(true, BuildSuccessfulSendNotification)).SetName("SuccessfulBuild_NotifyOnlyOnError_True");
+            yield return new TestCaseData(MockIIntegrationResultSuccess(BuildName), true, new Tuple<bool, SendNotification>(false, BuildSuccessfulSendNotification)).SetName("SuccessfulBuild_NotifyOnlyOnError_False");
+
+            // Fixed Cases
+            yield return new TestCaseData(MockIIntegrationResultFixed(BuildName), true, new Tuple<bool, SendNotification>(true, BuildFixedSendNotification)).SetName("FixedBuild_NotifyOnlyOnError_True");
+            yield return new TestCaseData(MockIIntegrationResultFixed(BuildName), false, new Tuple<bool, SendNotification>(true, BuildFixedSendNotification)).SetName("FixedBuild_NotifyOnlyOnError_False");
+
+            // Failure Cases
+            yield return new TestCaseData(MockIIntegrationResultFailure(BuildName, BuildFailureUrl, BuildFailureSingleUser), true, new Tuple<bool, SendNotification>(true, BuildFailedSingleSendNotification)).SetName("FailedBuild_NotifyOnlyOnError_True_SingleUser");
+            yield return new TestCaseData(MockIIntegrationResultFailure(BuildName, BuildFailureUrl, BuildFailureMultiUser), false, new Tuple<bool, SendNotification>(true, BuildFailedMultiSendNotification)).SetName("FailedBuild_NotifyOnlyOnError_False_MultiUser");
+        }
+
+        private static IIntegrationResult MockIIntegrationResultSuccess(string projectName)
+        {
+            Mock<IIntegrationResult> result = new Mock<IIntegrationResult>(MockBehavior.Strict);
+
+            result.SetupGet(x => x.ProjectName).Returns(projectName);
+            result.SetupGet(x => x.Succeeded).Returns(true);
+            result.SetupGet(x => x.Fixed).Returns(false);
+            result.SetupGet(x => x.Failed).Returns(false);
+
+            return result.Object;
+        }
+
+        private static IIntegrationResult MockIIntegrationResultFixed(string projectName)
+        {
+            Mock<IIntegrationResult> result = new Mock<IIntegrationResult>(MockBehavior.Strict);
+
+            result.SetupGet(x => x.ProjectName).Returns(projectName);
+            result.SetupGet(x => x.Succeeded).Returns(true);
+            result.SetupGet(x => x.Fixed).Returns(true);
+            result.SetupGet(x => x.Failed).Returns(false);
+
+            return result.Object;
+        }
+
+        private static IIntegrationResult MockIIntegrationResultFailure(string projectName, string projectURL, IEnumerable<string> failingUsers)
+        {
+            Mock<IIntegrationResult> result = new Mock<IIntegrationResult>(MockBehavior.Strict);
+
+            result.SetupGet(x => x.ProjectName).Returns(projectName);
+            result.SetupGet(x => x.Succeeded).Returns(false);
+            result.SetupGet(x => x.Fixed).Returns(false);
+            result.SetupGet(x => x.Failed).Returns(true);
+            result.SetupGet(x => x.ProjectUrl).Returns(projectURL);
+            result.SetupGet(x => x.FailureUsers).Returns(new ArrayList(failingUsers.ToArray()));
+
+            return result.Object;
+        }
+    }
+}
---
+++ ccnet.hipchat.v2.plugin/HipChatPublisher.cs
@@ -0,0 +1,179 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HipChatPublisher.cs" company="Ace Olszowka">
+// Copyright (c) 2015 Ace Olszowka.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ccnet.hipchat.v2.plugin
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Exortech.NetReflector;
+    using HipChat.Net;
+    using HipChat.Net.Http;
+    using HipChat.Net.Models.Request;
+    using ThoughtWorks.CruiseControl.Core;
+
+    /// <summary>
+    /// A CruiseControl.NET Publisher that Publishes to HipChat.
+    /// </summary>
+    [ReflectorType("hipchat")]
+    public class HipChatPublisher : ITask
+    {
+        /// <summary>
+        /// Gets or sets the OAuth 2 bearer token for use with the HipChat v2 API.
+        /// </summary>
+        /// <remarks>See https://hipchat.com/account/api for more information or to generate your key.</remarks>
+        [ReflectorProperty("auth-token")]
+        public string AuthToken { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Room Name to receive notification.
+        /// </summary>
+        [ReflectorProperty("room-name")]
+        public string RoomName { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether or not a notification should only be sent on error.
+        /// </summary>
+        /// <remarks>
+        ///     It is recommended that you set this property, otherwise you
+        /// will be notified of every successful build which can become very
+        /// chatty.
+        ///     You will still be notified when a build is "Fixed" if the build
+        /// was previously broken, which in most scenarios is what you want. In
+        /// this iteration there is no way to turn off these fixed alerts.
+        /// </remarks>
+        [ReflectorProperty("notify-only-on-error", Required = false)]
+        public bool NotifyOnlyOnError { get; set; }
+
+        /// <summary>
+        /// Executed when this Publisher is Invoked.
+        /// </summary>
+        /// <param name="result">The result of the Integration being ran.</param>
+        public void Run(IIntegrationResult result)
+        {
+            Tuple<bool, SendNotification> evaluatedIntergration = EvaluateIntegration(result, this.NotifyOnlyOnError);
+
+            if (evaluatedIntergration.Item1)
+            {
+                var hipchatClient =
+                    new HipChatClient(new ApiConnection(new Credentials(this.AuthToken)));
+
+                // Wait for the message to be posted
+                hipchatClient.Rooms.SendNotificationAsync(this.RoomName, evaluatedIntergration.Item2).Wait();
+            }
+        }
+
+        /// <summary>
+        /// Evaluate the given <see cref="IIntegrationResult"/> for the appropriate HipChatPublisher action.
+        /// </summary>
+        /// <param name="result">The <see cref="IIntegrationResult"/>.</param>
+        /// <param name="notifyOnlyOnError">Indicates whether or not we should only notify on error.</param>
+        /// <returns>
+        /// A Tuple where the first value indicates whether or not anything
+        /// should be posted to HipChat and the second value contains the
+        /// message to post.
+        /// </returns>
+        internal static Tuple<bool, SendNotification> EvaluateIntegration(IIntegrationResult result, bool notifyOnlyOnError)
+        {
+            bool shouldSendRoomNotification = true;
+            SendNotification message = null;
+
+            if (result.Failed)
+            {
+                message = IntegrationFailedMessage(result);
+            }
+            else if (result.Succeeded || result.Fixed)
+            {
+                // The check for Fixed here is intentional, the CruiseControl.NET
+                // Documentation is not clear on if you are always guaranteed to
+                //  Have a Succeeded build that is also Fixed.
+                if (result.Fixed)
+                {
+                    message = IntegrationFixedMessage(result);
+                }
+                else
+                {
+                    // This success was "yet another successful build" check
+                    // Check to see if we should log these.
+                    if (notifyOnlyOnError)
+                    {
+                        shouldSendRoomNotification = false;
+                    }
+
+                    message = IntegrationSuccessfulMessage(result);
+                }
+            }
+            else
+            {
+                // In theory we should never hit this, but is probably useful
+                // when attempting to port this to different CCNET Versions.
+                System.Diagnostics.Trace.WriteLine("Unknown Integration Status!");
+            }
+
+            return new Tuple<bool, SendNotification>(shouldSendRoomNotification, message);
+        }
+
+        /// <summary>
+        /// Generates the message displayed in HipChat when the Integration fails.
+        /// </summary>
+        /// <param name="result">The <see cref="IIntegrationResult"/>.</param>
+        /// <returns>The message to be sent to HipChat when the Integration fails.</returns>
+        internal static SendNotification IntegrationFailedMessage(IIntegrationResult result)
+        {
+            // Append @ for Mentions, Currently this is broken, but a message is out to
+            // HipChat Support to see if we can get support for this when using an HTML
+            IEnumerable<string> mentions =
+                result.FailureUsers.ToArray().Select(failureUser => "@" + failureUser);
+            string failureUsers = string.Join(",", mentions);
+
+            SendNotification failedMessage =
+                new SendNotification()
+                {
+                    Color = MessageColor.Red,
+                    Message = string.Format("<b>[FAILED]</b> {0} <a href=\"{1}\">Build Log</a>. Breakers: {2}", result.ProjectName, result.ProjectUrl, failureUsers),
+                    Notify = true
+                };
+
+            return failedMessage;
+        }
+
+        /// <summary>
+        /// Generates the message displayed in HipChat when the Integration is Fixed.
+        /// </summary>
+        /// <param name="result">The <see cref="IIntegrationResult"/>.</param>
+        /// <returns>The message to be sent to HipChat when the Integration is Fixed.</returns>
+        internal static SendNotification IntegrationFixedMessage(IIntegrationResult result)
+        {
+            SendNotification fixedMessage =
+                new SendNotification()
+                {
+                    Color = MessageColor.Green,
+                    Message = string.Format("<b>[FIXED]</b> {0}", result.ProjectName),
+                    Notify = true
+                };
+
+            return fixedMessage;
+        }
+
+        /// <summary>
+        /// Generates the message displayed in HipChat when the Integration is Successful.
+        /// </summary>
+        /// <param name="result">The <see cref="IIntegrationResult"/>.</param>
+        /// <returns>The message to be sent to HipChat when the Integration is Successful.</returns>
+        internal static SendNotification IntegrationSuccessfulMessage(IIntegrationResult result)
+        {
+            SendNotification successMessage =
+                new SendNotification()
+                {
+                    Color = MessageColor.Green,
+                    Message = string.Format("<b>[SUCCESSFUL]</b> {0}", result.ProjectName),
+                    Notify = true
+                };
+
+            return successMessage;
+        }
+    }
+}
